8cbcbb302f9d4c868b86ee9c8b209694248595f0
Elaine E. Kushkowski <elainekushkowski@Elaines-MacBook-Pro.local>
2017-12-11 22:47:13 -0800

    fixed post processing, now finds common nodes in steiner and shortest paths

--- post_processing.py
+++ post_processing.py
@@ -19,8 +19,13 @@ def main():
 	common_names_dict = read_common_names('nodes-flybase.txt')
 	steiner_terminal_nodes,steiner_non_terminal_nodes = read_tree_nodes('tree_nodes.txt')
 	#read_BFS_rank('BFS_rank.txt')
-	read_new_shortest_paths('new_shortest_paths.txt')
+	shortest_paths = read_new_shortest_paths('new_shortest_paths.txt')
+	steiner_nt_common = FB_to_common(steiner_non_terminal_nodes,common_names_dict)
+	new_shortest_common = FB_to_common(shortest_paths, common_names_dict)
 
+	both_set = in_both_sets(steiner_nt_common,new_shortest_common)
+
+	print('Both:', both_set)
 	return
 
 
@@ -77,9 +82,30 @@ def read_new_shortest_paths(filename):
 	print('Shortest Paths Nodes:', shortest_paths_nodes)
 	return shortest_paths_nodes
 
-def FB_to_common(node):
-	new_name = common_names_dict[node]
-	return new_name
+## converst 
+def FB_to_common(node_set, common_names_dict):
+	new_set = set()
+	for node in node_set:
+		if node in common_names_dict:
+			new_set.add(common_names_dict[node])
+		else:
+			new_set.add(node)
+	print(new_set)
+	return new_set
+
+def in_both_sets(set1, set2):
+	both_set = set()
+	if len(set1)>len(set2):
+		for node in set1:
+			if node in set2:
+				both_set.add(node)
+	else:
+		for node in set2:
+			if node in set1:
+				both_set.add(node)
+	return both_set
+
+
 
 
 if __name__ == '__main__':
